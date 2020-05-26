ebd1f2e8a75b0d6896d8db666ee82656a7fec481
rusydaniwildan <rusydaniwildan@student.uns.ac.id>
2020-05-26 16:09:47 +0700

    Create cobahotel.py

---
+++ cobahotel.py
@@ -0,0 +1,26 @@
+import csv
+
+def kamarHotel():
+    while True:
+        DATA_FILE_NAME = 'listharga.csv'
+
+    with open (DATA_FILE_NAME) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter= ',')
+        listharga = list(readCSV)
+        line_count = 0
+
+        for row in readCSV:
+            if line_count == 0:
+                print(f'Nama kolom : {row}')
+                line_count += 1
+            else:
+                print(row)
+                line_count += 1
+
+        print(f'Total {line_count} baris')
+    print(listharga)
+
+
+
+
+
