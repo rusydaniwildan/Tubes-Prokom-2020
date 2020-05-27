c15dfdd018b4766f10ca2663e32f2f48885716c7
hafidhalfa <65947137+hafidhalfa@users.noreply.github.com>
2020-05-27 22:20:19 +0700

    Update cobahotel.py

--- cobahotel.py
+++ cobahotel.py
@@ -1,11 +1,13 @@
+
+
+
+
 import os
 
 import datetime
 
 lama_hari=0
 
-def waktuinap():
-    x=datetime.datetime.now()
 
 def print_menu():
     print(1* "-")
@@ -24,69 +26,106 @@ def print_menu():
     print('\t','            Asri')
     print()
     print(50* "-")
+    print()
+    
+print_menu()
+
+ 
+def sistem():
     print('Pilih yang anda inginkan ?')
     print()
     print('1. CHECK IN')
-    print('2. keluar program')
-    print()
+    print('2. keluar program') 
+    pilihan = input("Pilih yang anda inginkan (1/2):  ")
+    if pilihan == "1":
+        print("Dimohon mengisi tanggal")
+    else :
+        quit()
+sistem()
+    
+    
+def waktuinap(): 
+    print('\t','Masukkan tanggal CHECK-IN')
+    tanggalmasuk = int(input("Masukkan TANGGAL (1-31):"))
+    bulanmasuk = int(input("Masukkan BULAN (1-12):"))
+    tahunmasuk = int(input("Masukkan TAHUN (2020-2050):"))
+    
+    print('\t','Masukkan tanggal CHECK-OUT')
+    tanggalkeluar = int(input("Masukkan TANGGAL (1-31):"))
+    bulankeluar = int(input("Masukkan BULAN (1-12):"))
+    tahunkeluar = int(input("Masukkan TAHUN (2020-2050):"))
+    
+    keluar = datetime.datetime(tahunkeluar, bulankeluar, tanggalkeluar)
+    masuk = datetime.datetime(tahunmasuk, bulanmasuk, tanggalmasuk)
+    lama = keluar - masuk
+    print("Anda menginap selama", lama)
+    
+waktuinap()
+
+
+def jumlahorang():
+    Dewasa = input("Masukkan jumlah dewasa(0-5): ")
+    Anak = input("Masukan jumlah anak(0-5) ")
+    Bayi = input("Masukkan jumlah bayi(0-5) ")
+    print("Jumlah penghuni kamar sebanyak", Dewasa, "dewasa, ", Anak, "anak, dan ", Bayi, "bayi")
+jumlahorang()
+    
+
+def jenis():   
     print('Jenis kamar hotel?')
     print()
     print('1. Reguler')
     print('2. Deluxe')
     print('3. Suite')
     print()
-    
-    tanggalmasuk = int(input("Masukkan TANGGAL (1-31):"))
-    bulanmasuk = int(input("Masukkan BULAN (1-12):"))
-    tahunmasuk = int(input("Masukkan TAHUN (2020-2050):"))
-    
-    keluar = datetime.datetime.now()
-    masuk = datetime.datetime(tahunmasuk, bulanmasuk, tanggalmasuk)
-    lama = keluar - masuk
-    
-    
-
-
-print_menu()
-pilihan = input("Pilih yang anda inginkan:  ")
-    
-    
-jenis = input("Jenis kamar hotel? ")
-if jenis == "1":
-    bed = input("Queen atau Twin? ")
-    if bed == "Queen":
-        from random import randint
-        for _ in range(450):
-            harga = randint(400, 450)
-        print("Tarif",harga,".000 rupiah")
-    if bed == "Twin":
-        from random import randint
-        for _ in range(460):
-            harga = randint(420, 460)
-        print("Tarif",harga,".000 rupiah")
         
-elif jenis == "2":
-    bed = input("Queen atau Twin? ")
-    if bed == "Queen":
-        from random import randint
-        for _ in range(700):
-            harga = randint(650, 700)
-        print("Tarif",harga,".000 rupiah")
-    if bed == "Twin":
-        from random import randint
-        for _ in range(720):
-            harga = randint(680, 720)
-            print("Tarif",harga,".000 rupiah")
+    jenis = input("Jenis kamar hotel? ")
+    if jenis == "1":
+        bed = input("Queen atau Twin? ")
+        if bed == "Queen":
+            from random import randint
+            for _ in range(1):
+                harga = randint(400, 450)
+                print("Tarif",harga,".000 rupiah")
+        if bed == "Twin":
+            from random import randint
+            for _ in range(1):
+                harga = randint(420, 460)
+                print("Tarif",harga,".000 rupiah")
         
-if jenis == "3":
-    bed = input("Queen atau Twin? ")
-    if bed == "Queen":
-        from random import randint
-        for _ in range(960):
-            harga = randint(900, 960)
-            print("Tarif",harga,".000 rupiah")
-    if bed == "Twin":
-        from random import randint
-        for _ in range(980):
-            harga = randint(920, 980)
-            print("Tarif",harga,".000 rupiah")
+    elif jenis == "2":
+        bed = input("Queen atau Twin? ")
+        if bed == "Queen":
+             from random import randint
+             for _ in range(1):
+                harga = randint(650, 700)
+                print("Tarif",harga,".000 rupiah")
+        if bed == "Twin":
+            from random import randint
+            for _ in range(1):
+                harga = randint(680, 720)
+                print("Tarif",harga,".000 rupiah")
+        
+    if jenis == "3":
+        bed = input("Queen atau Twin? ")
+        if bed == "Queen":
+            from random import randint
+            for _ in range(1):
+                harga = randint(900, 960)
+                print("Tarif",harga,".000 rupiah")
+        if bed == "Twin":
+            from random import randint
+            for _ in range(1):
+                harga = randint(920, 980)
+                print("Tarif",harga,".000 rupiah")
+jenis()
+
+iterasi = True
+print("WAJIB MEMPUNYAI IDENTITAS JIKA AKAN MELAKUKAN PEMBAYARAN!")
+
+while iterasi == True:
+    identitas = input("Apakah anda mempunyai identitas(KTP/Paspor/SIM)? (y/n)")
+    if identitas =="y":
+        iterasi = False
+    else :
+        iterasi = True
